406b2d109bdff2615c3a8e61d6f2e8c0bf9405ae
matt.retzer <matthew.retzer@macmillan.com>
2016-06-07 11:41:05 -0400

    finished updateing tmparchive

--- validator_tmparchive.rb
+++ validator_tmparchive.rb
@@ -1,15 +1,12 @@
 ENV["NLS_LANG"] = "AMERICAN_AMERICA.WE8MSWIN1252"
 
-## FOR TESTING  undo these for production, and untoggle lines:  12/13, 27/28, 30/31
-
 require 'fileutils'
 require 'logger'
 require 'find'
 require 'oci8'
 require 'to_xml'
 require 'json'
-#require_relative '../utilities/oraclequery.rb'
-require_relative '../../../bookmaker-dev/utilities/oraclequery.rb'
+require_relative '../utilities/oraclequery.rb'
 
 # ---------------------- VARIABLES (HEADER)
 unescapeargv = ARGV[0].chomp('"').reverse.chomp('"').reverse
@@ -17,7 +14,7 @@ input_file = File.expand_path(unescapeargv)
 input_file = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).join(File::SEPARATOR)
 filename_split = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).pop
 input_file_normalized = input_file.gsub(/ /, "")
-filename_normalized = string.scan( /[^[:alnum:]\._-]/ ) { |badchar| string=string.tr(badchar,'') } #strip any chars but alphanumeric, plus these 3 .-_
+filename_normalized = filename_split.scan( /[^[:alnum:]\._-]/ ) { |badchar| filename_split=filename_split.tr(badchar,'') } #strip any chars but alphanumeric, plus these 3 .-_
 basename_normalized = File.basename(filename_normalized, ".*")
 extension = File.extname(filename_normalized)
 project_dir = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact))[0...-2].join(File::SEPARATOR)
@@ -25,11 +22,12 @@ project_name = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARA
 inbox = File.join(project_dir, 'IN')
 outbox = File.join(project_dir, 'OUT')
 working_dir = File.join('S:', 'validator_tmp')
-#working_dir = 
 tmp_dir=File.join(working_dir, basename_normalized)
+working_file = File.join(tmp_dir, filename_normalized)
+bookinfo_file = File.join(tmp_dir,'book_info.json')
 testing_value_file = File.join("C:", "staging.txt")
-#testing_value_file = File.join("C:", "nothing.txt")  #for testing
-json_file = File.join(tmp_dir,'info.json')
+inprogress_file = File.join(inbox,"#{filename_normalized}_IN_PROGRESS.txt")
+errFile = File.join(inbox, "ERROR_RUNNING_#{filename_normalized}.txt")
 
 # ---------------------- LOGGING
 logfolder = File.join(working_dir, 'logs')
@@ -41,10 +39,6 @@ end
 FileUtils.mkdir_p logfolder
 
 
-# ---------------------- LOCAL VARIABLES
-isbn_namecheck = false
-isbn_lookupcheck = false
-
 #--------------------- RUN
 #clean up dummy logs from previous run:
 Find.find(logfolder) { |file|
@@ -54,59 +48,51 @@ Find.find(logfolder) { |file|
 logger.info "############################################################################"
 logger.info('validator_tmparchive') {"file \"#{filename_normalized}\" was dropped into the #{project_name} folder"}
 
-# check for isbn in filename:
-if filename_normalized =~ /9(78|-78|7-8|78-|-7-8)[0-9,-]{10,14}/
-    isbn_check = true
-    isbn = filename_normalized.match(/9(78|-78|7-8|78-|-7-8)[0-9,-]{10,14}/).to_s.tr('-','').slice(0..12)
-end
+puts filename_normalized
+#test filename for isbn and fyle type for =~ .doc
+if extension =~ /.doc/ && filename_normalized =~ /9(78|-78|7-8|78-|-7-8)[0-9,-]{10,14}/
 
-#lookup info on isbn at data warehouse
-thissql = personSearchSingleKey(isbn, "EDITION_EAN", "Production Manager")
-myhash = runPeopleQuery(thissql)
-
-unless myhash.nil? or myhash.empty? or !myhash or myhash['book'].nil? or myhash['book'].empty? or !myhash['book']
-    isbn_lookupcheck = true
-    thissql_B = personSearchSingleKey(isbn, "EDITION_EAN", "Production Editor")
-    myhash_B = runPeopleQuery(thissql_B)
-    #puts "DB Connection SUCCESS: Found a PM record"
-
-    #WRITING TO JSON
-    datahash = {}
-    datahash.merge!(production_editor: myhash_B['book']['PERSON_REALNAME'][0])
-    datahash.merge!(production_manager: myhash['book']['PERSON_REALNAME'][0])
-    datahash.merge!(title: myhash['book']['WORK_COVERTITLE'][0])
-    datahash.merge!(author: myhash['book']['WORK_COVERAUTHOR'][0])
-    datahash.merge!(product_type: myhash['book']['PRODUCTTYPE_DESC'][0])
-    datahash.merge!(imprint: myhash['book']['IMPRINT_DISPLAY'][0])
-    finaljson = JSON.generate(datahash)
-
-    # Printing the final JSON object
-    File.open(json_file, 'w+:UTF-8') do |f|
-      f.puts finaljson
-    end
-else
-    puts "No DB record found; removing author links for addons"
-end
-
-
-
-if extension =~ /.doc/
-	logger.info('validator_tmparchive') {"\"#{basename_normalized}\" is a .doc or .docx, moving to tmpdir"}
-    tmp_dir=File.join(working_dir, basename_normalized) 
-    working_file = File.join(tmp_dir, filename_normalized)
-    inprogress_file = File.join(inbox,"#{filename_normalized}_IN_PROGRESS.txt")
+    #move file into tmparchive
+    logger.info('validator_tmparchive') {"\"#{basename_normalized}\" is a .doc or .docx with isbn in title, moving to tmpdir"}
     FileUtils.mkdir_p tmp_dir
     File.open(inprogress_file, 'w') { |f|
         f.puts "Processing in progress for file #{filename_normalized}."
     }
     FileUtils.cp input_file, working_file
+
+    #check isbn against data-warehouse
+    isbn = filename_normalized.match(/9(78|-78|7-8|78-|-7-8)[0-9-]{10,14}/).to_s.tr('-','').slice(0..12)
+    thissql = personSearchSingleKey(isbn, "EDITION_EAN", "Production Manager")
+    myhash = runPeopleQuery(thissql)
+
+    #verify that data warehouse returned something
+    if myhash.nil? or myhash.empty? or !myhash or myhash['book'].nil? or myhash['book'].empty? or !myhash['book'] 
+        logger.info('validator_tmparchive') {"data warehouse lookup on isbn \"#{isbn}\"failed, skipping write to json"}
+
+    else  #lookup was good, continue: 
+        logger.info('validator_tmparchive') {"data warehouse lookup PM for isbn \"#{isbn}\"succeeded, looking up PE, writing to json"}
+        thissql_B = personSearchSingleKey(isbn, "EDITION_EAN", "Production Editor")
+        myhash_B = runPeopleQuery(thissql_B)
+        datahash = {}
+        datahash.merge!(production_editor: myhash_B['book']['PERSON_REALNAME'][0])
+        datahash.merge!(production_manager: myhash['book']['PERSON_REALNAME'][0])
+        datahash.merge!(title: myhash['book']['WORK_COVERTITLE'][0])
+        datahash.merge!(author: myhash['book']['WORK_COVERAUTHOR'][0])
+        datahash.merge!(product_type: myhash['book']['PRODUCTTYPE_DESC'][0])
+        datahash.merge!(imprint: myhash['book']['IMPRINT_DISPLAY'][0])
+        finaljson = JSON.generate(datahash)
+
+        # Printing final JSON object
+        File.open(bookinfo_file, 'w+:UTF-8') do |f|
+          f.puts finaljson
+        end
+    end    
 elsif filename_normalized =~ /^.*_IN_PROGRESS.txt/ || filename_normalized =~ /ERROR_RUNNING_.*.txt/
 	logger.info('validator_tmparchive') {"ignoring our own .txt outfile"}
 else
-    logger.info('validator_tmparchive') {"This is not a .doc or .docx file, posting error.txt to the inbox for user."}
-    errFile = File.join(inbox, "ERROR_RUNNING_#{filename_normalized}.txt")
+    logger.info('validator_tmparchive') {"This is not a .doc or .docx file or filename contains no isbn, posting error.txt to the inbox for user."}
     File.open(errFile, 'w') { |f|
-        f.puts "Unable to process \"#{filename_normalized}\", It is not a .doc or .docx."
+        f.puts "Unable to process \"#{filename_normalized}\". Either it is not a .doc or .docx file, or no ISBN was included in the filename."
     }
 end
 
