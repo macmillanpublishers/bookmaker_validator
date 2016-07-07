bee61638a2b2530c956ec6bd03cca31f2bb2ac7d
matt.retzer <matthew.retzer@macmillan.com>
2016-07-07 08:19:55 -0400

    setting up header for test

--- val_header.rb
+++ val_header.rb
@@ -1,9 +1,10 @@
 require 'fileutils'
+require 'logger'
 
-require_relative 'utilities/mcmlln-tools.rb'
+#require_relative 'utilities/mcmlln-tools.rb'
 
-module Vldtr
-	class Project
+module Val
+	class Doc
 		@unescapeargv = ARGV[0].chomp('"').reverse.chomp('"').reverse
   		@input_file = File.expand_path(@unescapeargv)
   		@@input_file = @input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).join(File::SEPARATOR)
@@ -30,6 +31,23 @@ module Vldtr
 		def self.extension
 			@@extension
 		end
+	end	
+	class AbsolutePaths
+		@@working_dir = File.join('S:', 'validator_tmp')
+		def self.working_dir
+			@@working_dir
+		end
+		#@@validator_dir = File.dirname(__FILE__)
+		@@validator_dir = File.join('S:', 'resources', 'bookmaker_scripts', 'bookmaker_validator')
+		def self.validator_dir
+			@@validator_dir
+		end			
+		@@testing_value_file = File.join("C:", "staging.txt")
+		def self.testing_value_file
+			@@testing_value_file
+		end
+	end	
+	class Paths 
 		@@project_dir = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact))[0...-2].join(File::SEPARATOR)
 		def self.project_dir
 			@@project_dir
@@ -37,107 +55,73 @@ module Vldtr
 		@@project_name = input_file.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact))[0...-2].pop
 		def self.project_name
 			@@project_name
-		end		
+		end
+		@@inbox = File.join(project_dir, 'IN')
+		def self.inbox
+			@@inbox
+		end
+		@@outbox = File.join(project_dir, 'OUT')
+		def self.outbox
+			@@outbox
+		end
+		@@tmp_dir=File.join(working_dir, basename_normalized)
+		def self.tmp_dir
+			@@tmp_dir
+		end
+		@@mailer_dir = File.join(validator_dir,'mailer_messages')		
+		def self.mailer_dir
+			@@mailer_dir
+		end
+	end	
+	class Files
+		@@working_file = File.join(tmp_dir, filename_normalized)
+		def self.working_file
+			@@working_file
+		end	
+		@@bookinfo_file = File.join(tmp_dir,'book_info.json')
+		def self.bookinfo_file
+			@@bookinfo_file
+		end
+		@@stylecheck_file = File.join(tmp_dir,'style_check.json')
+		def self.stylecheck_file
+			@@stylecheck_file
+		end	
+		@@contacts_file = File.join(tmp_dir,'contacts.json')
+		def self.contacts_file
+			@@contacts_file
+		end	
+		@@status_file = File.join(tmp_dir,'status_info.json') 
+		def self.status_file
+			@@status_file
+		end	
+		@@inprogress_file = File.join(inbox,"#{Doc.filename_normalized}_IN_PROGRESS.txt")
+		def self.inprogress_file
+			@@inprogress_file
+		end	
+		@@errFile = File.join(project_dir, "ERROR_RUNNING_#{Doc.filename_normalized}.txt")
+		def self.errFile
+			@@errFile
+		end	
 	end
-
-#working my way from the top, I am up to here, for paths:
-inbox = File.join(project_dir, 'IN')
-outbox = File.join(project_dir, 'OUT')
-working_dir = File.join('S:', 'validator_tmp')
-tmp_dir=File.join(working_dir, basename_normalized)
-validator_dir = File.dirname(__FILE__)
-mailer_dir = File.join(validator_dir,'mailer_messages')
-working_file = File.join(tmp_dir, filename_normalized)
-bookinfo_file = File.join(tmp_dir,'book_info.json')
-stylecheck_file = File.join(tmp_dir,'style_check.json')
-contacts_file = File.join(tmp_dir,'contacts.json')
-status_file = File.join(tmp_dir,'status_info.json') 
-testing_value_file = File.join("C:", "staging.txt")
-#inprogress_file = File.join(inbox,"#{filename_normalized}_IN_PROGRESS.txt")
-errFile = File.join(project_dir, "ERROR_RUNNING_#{filename_normalized}.txt")
-
-
-  class Paths
-	    def self.tmp_dir
-	      $tmp_dir
-	    end
-
-	    def self.log_dir
-	      $log_dir
-	    end
-
-	    def self.scripts_dir
-	      $scripts_dir
-	    end
-
-	    def self.resource_dir
-	      $resource_dir
-	    end
-
-	    # The location where each bookmaker component lives.
-		@@core_dir = File.join(scripts_dir, "bookmaker", "core")
-		def self.core_dir
-			@@core_dir
+	class Resources
+		@@thisscript = File.basename($0,'.rb')
+		def self.thisscript
+			@@thisscript
 		end
-
-		# Path to the submitted_assets directory
-		def self.submitted_images
-			if $assets_dir
-				$assets_dir
-			else 
-				Project.input_dir
-			end
+	end	
+	class Logs
+		@@this_dir = File.expand_path(File.dirname(__FILE__))
+		@@logfolder = File.join(AbsolutePaths.working_dir, 'logs')		#defaults for logging
+		@@logfilename = "#{Doc.basename_normalized}_log.txt"	
+		def self.log_setup(folder=@@logfolder,file=@@logfilename)		#can be overwritten in function call
+			logfile = File.join(folder,file)
+			@@logger = Logger.new(logfile)
+			def self.logger
+				@@logger
+			end	
+			logger.formatter = proc do |severity, datetime, progname, msg|
+			  "#{datetime}: #{Resources.thisscript} -- #{msg}\n"
+			end		
 		end
-
-		# Path to the temporary working directory
-		@@project_tmp_dir = File.join(tmp_dir, Project.filename)
-		def self.project_tmp_dir
-			@@project_tmp_dir
-		end
-
-		# Path to the images subdirectory of the temporary working directory
-		@@project_tmp_dir_img = File.join(project_tmp_dir, "images")
-		def self.project_tmp_dir_img
-			@@project_tmp_dir_img
-		end
-		
-		# Full path to outputtmp.html file
-		@@outputtmp_html = File.join(project_tmp_dir, "outputtmp.html")
-		def self.outputtmp_html
-			@@outputtmp_html
-		end
-
-		# Full path and filename for the normalized (i.e., spaces removed) input file in the temporary working dir
-		@@project_tmp_file = File.join(project_tmp_dir, Project.filename_normalized)
-		def self.project_tmp_file
-			@@project_tmp_file
-		end
-		
-		# Full path and filename for the .docx file
-		@@project_docx_file = File.join(project_tmp_dir, "#{Project.filename}.docx")
-		def self.project_docx_file
-			@@project_docx_file
-		end
-
-		# Full path and filename for the "in use" alert that is created
-		@@alert = File.join(Project.working_dir, "IN_USE_PLEASE_WAIT.txt")
-		def self.alert
-			@@alert
-		end
-
-		# Full path and filename for the "done" directory in Project working directory
-		def self.done_dir
-			if $done_dir
-				$done_dir
-			else 
-				Project.input_dir
-			end
-		end
-
-		# Full path to project log file
-		@@log_file = File.join(log_dir, "#{Project.filename}.txt")
-		def self.log_file
-			@@log_file
-		end
-	end
+	end	
 end
