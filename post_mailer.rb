387fdb841cdf88ad214db384a8c7666e6176c300
unknown <padwoadmin@NYAUTOMATIONSTG.newyork.hbpub.net>
2016-07-25 11:36:03 -0400

    fixed typo, edited bookinfostring for mailer

--- post_mailer.rb
+++ post_mailer.rb
@@ -30,14 +30,14 @@ to_address = 'To: '
 if File.file?(Val::Posts.bookinfo_file)
 	bookinfo_hash = Mcmlln::Tools.readjson(Val::Posts.bookinfo_file)
 	work_id = bookinfo_hash['work_id']
-	bookinfo_author = bookinfo_hash['bookinfo_author']
-	bookinfo_title = bookinfo_hash['bookinfo_title']
-	bookinfo_imprint = bookinfo_hash['bookinfo_imprint']
+	bookinfo_author = bookinfo_hash['author']
+	bookinfo_title = bookinfo_hash['title']
+	bookinfo_imprint = bookinfo_hash['imprint']
 	product_type = bookinfo_hash['product_type']
 	bookinfo_isbn = bookinfo_hash['isbn']
 	bookinfo_pename = bookinfo_hash['production_editor']
 	bookinfo_pmname = bookinfo_hash['production_manager']
-	bookinfo = "ISBN lookup for #{bookinfo_isbn}:\nbookinfo_title: \"#{bookinfo_title}\"\nbookinfo_author: \'#{bookinfo_author}\'\nbookinfo_imprint: \'#{bookinfo_imprint}\'\nPRODUCT-TYPE: \'#{product_type}\'\n"
+	bookinfo = "ISBN lookup for #{bookinfo_isbn}:\ntitle: \"#{bookinfo_title}\"\nauthor: \'#{bookinfo_author}\'\nimprint: \'#{bookinfo_imprint}\'\nproduct-type: \'#{product_type}\'\n"
 else
 	send_ok = false
 	logger.info {"bookinfo.json not present or unavailable, unable to determine what to send"}
@@ -147,8 +147,8 @@ MESSAGE_END
 
 		#sending another email, for Patrick to QA epubs
 		body_b = Val::Resources.mailtext_gsubs(epubQA_request_txt, warnings, errors, bookinfo)
-		
-message_epubQA = = <<MESSAGE_END_C
+
+message_epubQA = <<MESSAGE_END_C
 From: Workflows <workflows@macmillan.com>
 #{body_b}
 MESSAGE_END_C
