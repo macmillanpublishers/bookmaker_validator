d6febadda169fb2125402faf6a4b426b92a030c9
MacmillanWorkflows <workflows@macmillan.com>
2016-09-26 15:47:35 -0400

    adding handling for single-quotes in filenames to validator/egalleymaker

--- deploy_validator.bat
+++ deploy_validator.bat
@@ -1,3 +1,3 @@
 set "logfile=C:\Users\padwoadmin\Dropbox (Macmillan Publishers)\bookmaker_logs\bookmaker_validator\std_out-err_logs\%~n1_batch_outerr.txt"
 
-C:\Ruby200\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_validator\deploy_validator.rb '%1' >> "%logfile%" 2>&1
+C:\Ruby200\bin\ruby.exe S:\resources\bookmaker_scripts\bookmaker_validator\deploy_validator.rb %1 >> "%logfile%" 2>&1
