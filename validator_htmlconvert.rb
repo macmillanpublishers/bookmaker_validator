da7249d1f6b5a0ea21df801b0a685582b7960c65
matt.retzer <matthew.retzer@macmillan.com>
2017-03-28 15:28:08 -0400

    adding one logger message

--- validator_htmlconvert.rb
+++ validator_htmlconvert.rb
@@ -37,6 +37,7 @@ end
 if Val::Hashes.status_hash['bookmaker_ready'] == true
 
   # convert .docx to html
+  @logger.info {"this file is bookmaker_ready, running htmlmaker"}
   localRunNode(htmlmaker, "#{Val::Files.working_file} #{Val::Paths.tmp_dir} #{styles_json} #{stylefunctions_js}", status_hash)
 
   # test if file was created, updated lofs and status hash
